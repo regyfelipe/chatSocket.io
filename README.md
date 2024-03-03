cfc737e9b0e93d868a48453d18100a1e36594b3b
regyfelipe <wyrzer@gmail.com>
2024-03-03 12:56:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Chat usando Socket.io
+
+Este é um simples aplicativo de chat usando Socket.io, HTML, CSS e JavaScript.
+
+## Pré-requisitos
+
+- Node.js instalado
+- npm (Node Package Manager) ou yarn
+
+## Como executar
+
+1. Clone este repositório:
+
+```bash
+git clone https://github.com/regyfelipe/chatSocket.io.git
+
+```
+## Detalhes da implementação
+1. Arquivos
+index.html: Contém a estrutura HTML do chat.
+style.css: Arquivo de estilo para o chat.
+server.js: Servidor Node.js que gerencia a comunicação via Socket.io.
+
+
+## Tecnologias utilizadas
+- Socket.io: Biblioteca JavaScript para comunicação em tempo real.
+- Express.js: Framework web para Node.js.
+- Bootstrap: Framework front-end para desenvolvimento de sites e aplicações web responsivas.
---
+++ server.js
@@ -0,0 +1,32 @@
+const { Socket } = require('dgram');
+const express = require('express')
+const path = require('path')
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+    res.render('index.html');
+});
+
+let messages = [];
+
+io.on('connection', Socket => {
+    console.log(`Socket conectado: ${Socket.id}`);
+
+
+    Socket.emit('previousMessages', messages);
+    
+    Socket.on('sendMessage', data => {
+        messages.push(data);
+        Socket.broadcast.emit('receivedMessage', data);
+    });
+});
+
+server.listen(3000)
